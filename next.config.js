9b6ef863936e4dbae8511b3dfb26886777c698a7
The Aman <urtheaman@gmail.com>
2022-03-22 20:46:40 +0530

    added publicRuntimeConfig and serverRuntimeConfig

--- next.config.js
+++ next.config.js
@@ -9,9 +9,11 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  env: {
+  serverRuntimeConfig: {
     mapbox_key:
       "pk.eyJ1IjoidXJ0aGVhbWFuIiwiYSI6ImNsMTI2MzVjNzAwMnkzbXJ6OWY4aTRvMXQifQ.Y79bto09mWZaw5AWbXlZIQ",
+  },
+  publicRuntimeConfig: {
     mapbox_style: "mapbox://styles/urtheaman/cl12773u4001g14l91i9pzsha",
   },
 };
