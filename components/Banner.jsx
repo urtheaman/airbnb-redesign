cfbbbd1747e3b412c4af4effff485a5724263fb6
The Aman <urtheaman@gmail.com>
2022-04-30 12:05:09 +0530

    Update Banner.jsx

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -15,7 +15,7 @@ const Banner = () => {
       <div className="text-black absolute top-1/2 transform -translate-y-1/2 w-full text-center">
         <p className="text-sm sm:text-xl">Not sure where to go? Perfect.</p>
         <button className="text-purple-500 bg-white py-4 px-10 font-bold rounded-full shadow-md mt-3 lg:mt-5 hover:shadow-xl active:scale-90 transition duration-150">
-          Search
+          Search places
         </button>
       </div>
     </div>
